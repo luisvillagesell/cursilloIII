02859f1754c618d9a3313344c13224ecca85bb3a
Luis Gómez <luisvillagesell@gmail.com>
2024-03-11 22:19:52 -0300

    area de triangulo

---
+++ scrypt.js
@@ -0,0 +1,5 @@
+let base =  parseFloat(prompt('Ingrese la base'));
+let  altura = parseFloat(prompt ('ingrese la altura'));
+let area =  (base * altura)/2;
+console.log(`El área del triángulo es ${area}`);
+alert(`El área del triángulo es ${area}`);
