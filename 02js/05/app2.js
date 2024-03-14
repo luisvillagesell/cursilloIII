776d9b282d838f9e7a8ca0a98a58039fd758c3ef
Luis Gómez <luisvillagesell@gmail.com>
2024-03-13 22:44:12 -0300

    cierre clase 23mar

--- 02js/05/app2.js
+++ 02js/05/app2.js
@@ -1,15 +1,15 @@
+//definimos las variables
 let numeroMaximo = 100;
 let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-//let palabraVeces = 'vez';
 let maximosIntentos = 10;
-
+//mostramos el número secreto en la consola
 console.log('El numero secreto es ' + numeroSecreto);
-
+//se le pide al usuario que ingrese un número
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = parseInt(prompt(`Escribe un número del 1 al ${numeroMaximo}:`));
-
+//comienza el proceso de comparación entre el número secreto y el numero de usuario
     console.log(numeroUsuario);
     if (numeroUsuario == numeroSecreto) {
         alert(`¡Acertaste! el numero secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
